a6e81354a3bcde18afa3b3d308a9f134ce123796
partha-bro <62046798+partha-bro@users.noreply.github.com>
2021-04-17 11:27:50 +0530

    Add files via upload

---
+++ push.sh
@@ -0,0 +1,89 @@
+echo My Repository updateing......................
+
+date 
+
+echo My Repository updateing......................
+
+date 
+
+echo =============================================
+
+echo ============== Budget-Plan ==================
+
+cd budget-plan/
+git pull origin main
+git push origin main
+
+echo =============================================
+
+echo ==============   Problem   ==================
+
+cd problem/
+git pull origin master
+git push origin master
+
+echo =============================================
+
+echo ==============    Resume   ==================
+
+cd resume/
+git pull origin master
+git push origin master
+
+echo =============================================
+
+echo ============== Study-Repo  ==================
+
+cd study-repo/
+git pull origin master
+git push origin master
+
+echo =============================================
+
+echo ============== Ticketing System  ==================
+
+cd ticketing_system/
+git pull origin master
+git push origin master
+
+echo =============================================
+
+echo ============== Bluk Mail  ==================
+
+cd bulk_mail/
+git pull origin master
+git push origin master
+
+echo =============================================
+
+echo ============== IOVM  ==================
+
+cd iovm/
+git pull origin master
+git push origin master
+
+echo =============================================
+
+echo ============== NcrBricks.com  ==================
+
+cd ncrbricks.com/
+git pull origin master
+git push origin master
+
+echo =============================================
+
+echo ============== PDF_Quiz  ==================
+
+cd pdf_quiz/
+git pull origin master
+git push origin master
+
+echo =============================================
+
+echo ============== HTML_Editor  ==================
+
+cd html_editor/
+git pull origin master
+git push origin master
+
+echo =============================================
